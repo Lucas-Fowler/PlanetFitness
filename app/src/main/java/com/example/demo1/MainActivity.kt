ef877853dff176bf84343721b6fdcff71f7d673e
1935539 <lafsd8565@gmail.com>
2024-03-11 10:05:01 -0700

    Finished the layout of the login page

--- app/src/main/java/com/example/demo1/MainActivity.kt
+++ app/src/main/java/com/example/demo1/MainActivity.kt
@@ -7,11 +7,29 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getSupportActionBar()?.hide()
+
+        val username : TextView = findViewById(R.id.username)
+        val password : TextView = findViewById(R.id.password)
+        //admin and admin
+
+        val loginbtn : MaterialButton = findViewById(R.id.loginbtn)
+
+//        loginbtn.setOnClickListener {
+//            if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
+//                //correct
+//                Toast.makeText(applicationContext, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
+//            } else run {
+//                //incorrect
+//                Toast.makeText(applicationContext, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show()
+//            }
+//        }
     }
 }
