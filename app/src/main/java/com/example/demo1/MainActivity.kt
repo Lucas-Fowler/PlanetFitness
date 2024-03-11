fa1983104f534e73580fe6c9ffa18995bdd79bd6
1935539 <lafsd8565@gmail.com>
2024-03-11 08:52:20 -0700

    Initial commit

---
+++ app/src/main/java/com/example/demo1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.demo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getSupportActionBar()?.hide()
+    }
+}
